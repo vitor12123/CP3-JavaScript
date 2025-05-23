01c6e61a6fb3abbdc3ebf8982831e3fdb32a5ee1
vitor12123 <vitorpallis@gmail.com>
2025-05-22 22:24:22 -0300

    add: melhorando  o javaScript

--- app.js
+++ app.js
@@ -11,7 +11,15 @@ function BTNClK() {
         let preCO = produtoPreco.value
         let categ = selecao.value
         let product = produtoMercado.value
-        if (preCO == '' || product == '') {
+        if (preCO === 0 || preCO === "" && product != '') {
+            let invalido = document.createElement('div')
+            invalido.innerHTML = `
+                <h1> o produto não pode ser igual a zero!!</h1>
+                `
+            avisos.append(invalido)
+        }
+
+        if (product == '') {
             let aviso = document.createElement("div")
             aviso.innerHTML = `
             <h1 id='avisado'> NEM UM CAMPO PODE ESTAR VAZIO! </h1>
@@ -19,9 +27,10 @@ function BTNClK() {
             avisos.append(aviso)
         }
         else {
+            avisos.remove()
             let listaItens = document.createElement("div")
             listaItens.innerHTML = `
-        <li> produto: ${product} , preço: $ ${preCO}, categoria: ${categ} </li>
+        <li> produto: ${product} - preço: $ ${preCO} - categoria: ${categ} </li>
         `
             lista.append(listaItens)
         }
